a847997b11b6312e6ad0ec38e06b39c32ffda037
Jonathan Bailey <jonathan.bailey13@imperial.ac.uk>
2019-03-04 22:25:28 +0000

    Initial Commit

---
+++ Dash/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Dash
+//
+//  Created by Jonathan Bailey on 04/03/2019.
+//  Copyright © 2019 jonathan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
